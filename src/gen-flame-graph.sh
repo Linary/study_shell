d2dc4468921c82aab1b93383b3103f99aeddabaf
liningrui <liningrui@baidu.com>
2019-06-13 14:42:09 +0800

    Merge profile.sh and gen-flame-graph.sh

--- src/gen-flame-graph.sh
+++ src/gen-flame-graph.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-# Usage: ./gen.sh collected-stacks.txt
+# Usage: ./gen-flame-graph.sh -p pid -t time -o output
 
-TMPSTACKS=/tmp/flamegraph-stacks-collapsed.txt
-TMPPALETTE=/tmp/flamegraph-palette.map
+while getopts "p:t:o:" arg; do
+    case ${arg} in
+        p) PID="$OPTARG" ;;
+        t) TIME="$OPTARG" ;;
+        o) OUTPUT="$OPTARG" ;;
+        ?) echo "Usage: $0 -p pid -t time(seconds) -o output" && exit 1 ;;
+    esac
+done
 
-./stackcollapse-jstack.pl $1 > $TMPSTACKS
+if [[ -z "$PID" || -z "$TIME" || -z "$OUTPUT" ]]; then
+    echo "Usage: $0 -p pid -t time(seconds) -o output"
+    exit 1
+fi
 
-# 1st run - hot: default
-./flamegraph.pl --cp $TMPSTACKS > stacks.svg
+# clean env
+rm -f "$OUTPUT"
 
-# 2nd run - blue: I/O
-cp palette.map $TMPPALETTE
-cat $TMPPALETTE | grep -v '\.read' | grep -v '\.write' | grep -v 'socketRead' | grep -v 'socketWrite' | grep -v 'socketAccept' > palette.map
-./flamegraph.pl --cp --colors=io $TMPSTACKS > stacks.svg
+echo "Getting stacktraces from process $PID... will stop on ^C or when the process exits."
 
-echo "Done! Now see the output in stacks.svg"
+# Collect stack info
+
+STACK_OUTPUT="flamegraph-stacks-out.txt"
+
+END_TIME=$((SECONDS+TIME))
+while [[ $SECONDS -lt ${END_TIME} ]]; do
+    jstack "$PID" >> "$STACK_OUTPUT" && sleep 0.1 || break
+done
+
+echo "Done! stacks saved to $STACK_OUTPUT"
+
+# Collapse stack and generate flame graph
+./stackcollapse-jstack.pl ${STACK_OUTPUT} | ./flamegraph.pl > ${OUTPUT}
+
+#rm -f palette.map
+rm -f ${STACK_OUTPUT}
+
+echo "Done! Now see the output in ${OUTPUT}"
